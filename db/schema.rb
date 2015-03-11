f4f8d8354ac545e03464a61845b41c837d4cfcb2
Xiang <zhuyuan.xiang@X0515.local>
2015-03-11 18:44:02 +0800

    translate

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150310095629) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
     t.string   "trackable_type"
@@ -26,9 +29,9 @@ ActiveRecord::Schema.define(version: 20150310095629) do
     t.datetime "updated_at"
   end
 
-  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
-  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
-  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -46,7 +49,7 @@ ActiveRecord::Schema.define(version: 20150310095629) do
     t.integer  "likes"
   end
 
-  add_index "stories", ["user_id"], name: "index_stories_on_user_id"
+  add_index "stories", ["user_id"], name: "index_stories_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -56,6 +59,7 @@ ActiveRecord::Schema.define(version: 20150310095629) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "users", ["uid"], name: "index_users_on_uid"
+  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
+  add_foreign_key "stories", "users"
 end
