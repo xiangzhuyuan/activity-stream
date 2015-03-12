fa8dd2667384cdd8bbe2d16b6d20d5930bde5b70
Xiang <zhuyuan.xiang@X0515.local>
2015-03-12 15:31:02 +0800

    make it better

--- app/models/story.rb
+++ app/models/story.rb
@@ -8,5 +8,5 @@ class Story < ActiveRecord::Base
   validates :body, presence: true
 
 
-  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
+  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : get_ }
 end
