352cfe3051b3d114ab8adb585f7605b2d4ef39bd
xiang zhyuan <xiangzhuyuan@gmail.com>
2015-04-22 17:01:10 +0800

    fix bug

--- app/models/story.rb
+++ app/models/story.rb
@@ -8,5 +8,5 @@ class Story < ActiveRecord::Base
   validates :body, presence: true
 
 
-  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : get_ }
+  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
 end
